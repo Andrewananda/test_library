e40b8241ef5083f62fca0e8927da2bab58153d8d
AndrewAnanda <andrewtiema@gmail.com>
2023-11-02 14:31:08 +0300

    fixes

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -25,7 +25,8 @@ public class MyLibrary: DynamicDialogDelegate {
         vc.delegate = self
         vc._navigationController = viewController.navigationController
         vc.modalPresentationStyle = .custom
-        vc.show()
+//        vc.show()
+        viewController.navigationController?.pushViewController(vc, animated: false)
     }
     
     
